55239a24eaffdad0fb1d671a0e5707e70763aa7b
Oleksandr Sidarok <sasha.sidarok@disroot.org>
2022-01-07 15:34:11 +0200

    add all filters

--- js/filters.js
+++ js/filters.js
@@ -7,21 +7,83 @@ const filterDataByConfig = (data, config) => {
 
   if (config.type !== ANY) {
     filteredData = filteredData.filter((it) => it.offer.type === config.type)
+  }
 
+  if (config.price !== ANY) {
+    filteredData = filteredData.filter((it) => {
+      const price = it.offer.price
+      if(config.price === 'middle') return (price > 10000 && price < 50000)
+      if(config.price === 'low') return (price < 10000)
+      if(config.price === 'high') return (price > 50000)
+    })
+  }
+
+  if (config.rooms !== ANY) {
+    filteredData = filteredData.filter((it) => {
+      return String(it.offer.rooms) === config.rooms
+    })
+  }
+
+  if (config.guests !== ANY) {
+    filteredData = filteredData.filter((it) => {
+      return String(it.offer.guests) === config.guests
+    })
+  }
+
+  if (config.wifi) {
+    filteredData = filteredData.filter((it) => it.offer.features.some((feature) => {
+      return feature === 'wifi'
+    }))
+  }
+  if (config.dishwasher) {
+    filteredData = filteredData.filter((it) => it.offer.features.some((feature) => {
+      return feature === 'dishwasher'
+    }))
+  }
+  if (config.parking) {
+    filteredData = filteredData.filter((it) => it.offer.features.some((feature) => {
+      return feature === 'parking'
+    }))
+  }
+  if (config.washer) {
+    filteredData = filteredData.filter((it) => it.offer.features.some((feature) => {
+      return feature === 'washer'
+    }))
+  }
+  if (config.elevator) {
+    filteredData = filteredData.filter((it) => it.offer.features.some((feature) => {
+      return feature === 'elevator'
+    }))
+  }
+  if (config.conditioner) {
+    filteredData = filteredData.filter((it) => it.offer.features.some((feature) => {
+      return feature === 'conditioner'
+    }))
   }
 
   return filteredData
 }
 
-const getConfig = () => {return {type: getNodeValue('#housingtype')}}
+const getConfig = () => {
+  return {
+    type: getNodeValue('#housingtype'),
+    price: getNodeValue('#housing-price'),
+    rooms: getNodeValue('#housing-rooms'),
+    guests: getNodeValue('#housing-guests'),
+    wifi: getNodeValue('#filter-wifi', 'checked'),
+    dishwasher: getNodeValue('#filter-dishwasher', 'checked'),
+    parking: getNodeValue('#filter-parking', 'checked'),
+    washer: getNodeValue('#filter-washer', 'checked'),
+    elevator: getNodeValue('#filter-elevator', 'checked'),
+    conditioner: getNodeValue('#filter-conditioner', 'checked'),
+  }
+}
 
 export const filter = (data) => {
   const filteredData = filterDataByConfig(data, getConfig())
   const form = document.querySelector('.map__filters')
-  console.log(form)
 
   form.addEventListener('change', () => {
-    console.log('ok!')
     const filteredData = filterDataByConfig(data, getConfig())
     removePins()
     setPins(filteredData)
